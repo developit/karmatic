2a119487a20b57a204c2866898b00f6ce7a1191b
Andre Wiggins <andrewiggins@live.com>
2020-08-11 17:31:20 -0700

    Remove webpack devDep and remove local tests

--- test/jest-style.test.js
+++
@@ -1,9 +0,0 @@
-describe('jest-style', () => {
-	describe('not.stringContaining', () => {
-		const expected = 'Hello world!';
-
-		it('matches if the received value does not contain the expected substring', () => {
-			expect('How are you?').toEqual(expect.not.stringContaining(expected));
-		});
-	});
-});
